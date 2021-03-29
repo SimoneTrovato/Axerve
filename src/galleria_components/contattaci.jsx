4639d9e29049d763d6dd7229a89cd2c019b177c4
Trovato Simone <trovato@omicronconsulting.it>
2021-03-29 12:33:13 +0200

    commit 3

--- src/galleria_components/contattaci.jsx
+++ src/galleria_components/contattaci.jsx
@@ -100,12 +100,40 @@ class Contattaci extends Component {
     this.setState({ contatta: true });
     this.setState({ zeroErrori: false });
   }
+  indietro() {
+    this.getBack();
+  }
+
   render() {
     const { show } = this.state;
     switch (this.state.contatta) {
       case false:
         return (
           <div>
+            <div className="d-grid gap-2 col-6 mx-auto">
+              <button
+                className="btn btn-primary"
+                type="button"
+                onClick={() => {
+                  this.getBack();
+                }}
+                id="bottone2"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-arrow-return-left"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M14.5 1.5a.5.5 0 0 1 .5.5v4.8a2.5 2.5 0 0 1-2.5 2.5H2.707l3.347 3.346a.5.5 0 0 1-.708.708l-4.2-4.2a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 8.3H12.5A1.5 1.5 0 0 0 14 6.8V2a.5.5 0 0 1 .5-.5z"
+                  />
+                </svg>
+              </button>
+            </div>
             <div
               id="signupb"
               class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2"
